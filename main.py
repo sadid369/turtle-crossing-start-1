db795e2eb6416746bee1210035a3793b0767d0df
Sadid369 <sadid.jones@gmail.com>
2022-06-01 21:59:33 +0600

    1/6/2022

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
+
+
 screen.listen()
 screen.onkey(player.go_up, "Up")
 game_is_on = True
@@ -18,3 +21,13 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     car_manager.move_cars()
+    for car in car_manager.all_cars:
+       if car.distance(player)<20:
+           game_is_on =False
+           scoreboard.game_over()
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_lavel()
+
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,22 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.lavel = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280,250)
+        self.update_scoreboard()
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Lavel {self.lavel}", align="left", font=FONT)
+    def increase_lavel(self):
+        self.lavel += 1
+        self.update_scoreboard()
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align="center", font=FONT)
+
